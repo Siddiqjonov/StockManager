c2e6ea7aa1fe2cb91d393406e89d8b35046dff9e
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-08-09 12:13:32 +0500

    migration were added and database were updated

---
+++ src/StockManager.Infrastructure/Persistence/Migrations/20250809071126_InitialCreate.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockManager.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasurementUnits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementUnits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiptDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptDocuments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShipmentDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ClientId = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipmentDocuments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShipmentDocuments_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Balances",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ResourceId = table.Column<long>(type: "bigint", nullable: false),
+                    MeasurementUnitId = table.Column<long>(type: "bigint", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Balances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Balances_MeasurementUnits_MeasurementUnitId",
+                        column: x => x.MeasurementUnitId,
+                        principalTable: "MeasurementUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Balances_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiptResources",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiptDocumentId = table.Column<long>(type: "bigint", nullable: false),
+                    ResourceId = table.Column<long>(type: "bigint", nullable: false),
+                    MeasurementUnitId = table.Column<long>(type: "bigint", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptResources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReceiptResources_MeasurementUnits_MeasurementUnitId",
+                        column: x => x.MeasurementUnitId,
+                        principalTable: "MeasurementUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReceiptResources_ReceiptDocuments_ReceiptDocumentId",
+                        column: x => x.ReceiptDocumentId,
+                        principalTable: "ReceiptDocuments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReceiptResources_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShipmentResources",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShipmentDocumentId = table.Column<long>(type: "bigint", nullable: false),
+                    ResourceId = table.Column<long>(type: "bigint", nullable: false),
+                    MeasurementUnitId = table.Column<long>(type: "bigint", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipmentResources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShipmentResources_MeasurementUnits_MeasurementUnitId",
+                        column: x => x.MeasurementUnitId,
+                        principalTable: "MeasurementUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ShipmentResources_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ShipmentResources_ShipmentDocuments_ShipmentDocumentId",
+                        column: x => x.ShipmentDocumentId,
+                        principalTable: "ShipmentDocuments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Balances_MeasurementUnitId",
+                table: "Balances",
+                column: "MeasurementUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Balances_ResourceId",
+                table: "Balances",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_Name",
+                table: "Clients",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeasurementUnits_Name",
+                table: "MeasurementUnits",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptDocuments_Number",
+                table: "ReceiptDocuments",
+                column: "Number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptResources_MeasurementUnitId",
+                table: "ReceiptResources",
+                column: "MeasurementUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptResources_ReceiptDocumentId",
+                table: "ReceiptResources",
+                column: "ReceiptDocumentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptResources_ResourceId",
+                table: "ReceiptResources",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_Name",
+                table: "Resources",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentDocuments_ClientId",
+                table: "ShipmentDocuments",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentDocuments_Number",
+                table: "ShipmentDocuments",
+                column: "Number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentResources_MeasurementUnitId",
+                table: "ShipmentResources",
+                column: "MeasurementUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentResources_ResourceId",
+                table: "ShipmentResources",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentResources_ShipmentDocumentId",
+                table: "ShipmentResources",
+                column: "ShipmentDocumentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Balances");
+
+            migrationBuilder.DropTable(
+                name: "ReceiptResources");
+
+            migrationBuilder.DropTable(
+                name: "ShipmentResources");
+
+            migrationBuilder.DropTable(
+                name: "ReceiptDocuments");
+
+            migrationBuilder.DropTable(
+                name: "MeasurementUnits");
+
+            migrationBuilder.DropTable(
+                name: "Resources");
+
+            migrationBuilder.DropTable(
+                name: "ShipmentDocuments");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
---
+++ src/StockManager.Presentation/Configurations/DatabaseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManager.Infrastructure.Persistence.DataContext;
+using System;
+
+namespace StockManager.Presentation.Configurations;
+
+public static class DatabaseConfiguration
+{
+    public static void ConfigureSqlServerDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
+
+        builder.Services.AddDbContext<MainContext>(options =>
+            options.UseSqlServer(connectionString));
+    }
+}
