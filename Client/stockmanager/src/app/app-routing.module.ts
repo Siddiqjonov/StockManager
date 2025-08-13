156b6a80dd1726b3c874c550c63b9d0abc6ce27c
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-08-13 23:51:23 +0500

    routing file were modified

--- Client/stockmanager/src/app/app-routing.module.ts
+++ Client/stockmanager/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './components/balance/balance.component';
+import { ReceiptComponent } from './components/receipt/receipt.component';
+import { ShipmentComponent } from './components/shipment/shipment.component';
+import { ClientComponent } from './components/client/client.component';
+import { MeasurementUnitComponent } from './components/measurement-unit/measurement-unit.component';
+import { ResourcesComponent } from './components/resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BalanceComponent, pathMatch: 'full'},
+  {path: 'recipts', component: ReceiptComponent},
+  {path: 'shipments', component: ShipmentComponent},
+  {path: 'clients', component: ClientComponent},
+  {path: 'measurement-unit', component: MeasurementUnitComponent},
+  {path: 'resources', component: ResourcesComponent},
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
